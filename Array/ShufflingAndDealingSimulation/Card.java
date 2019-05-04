f5cbf43d7aaa81fc60b2a1a94db5c5b1674f4045
RenanBa <renanbarbosadesouza@gmail.com>
2019-05-04 10:43:53 -0700

    Create DeckOfCards to represent a deck of playing cards

--- Array/ShufflingAndDealingSimulation/Card.java
+++ Array/ShufflingAndDealingSimulation/Card.java
@@ -3,7 +3,7 @@
 public class Card
 {
     private String face; // face of card ("Ace", "Deuce", ...)
-    private string suit; // suit of card ("Hearts", "Diammonds", ...)
+    private string suit; // suit of card ("Hearts", "Diamonds", ...)
 
     // two arguments constructor initializer card's face and suit
     public Card( String cardFace, String cardSuit )
